15b1a943065b99833d2447effa18ef01a7cc3c39
Shawn Mathen <sgmathen@gmail.com>
2022-12-01 19:50:35 -0600

    Completed Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,15 @@
 
 mostTotal = 0
 total = 0
+all_sums = []
 with open("input.txt") as file:
     for row in file.read().strip().split("\n"):
         if row == "":
-            mostTotal = max(mostTotal, total)
+            # mostTotal = max(mostTotal, total)
+            all_sums .append(total)
             total = 0
         else:
             total += int(row)
 
-
-print(mostTotal)
+in_order = sorted(all_sums, reverse=True)
+print(sum(in_order[:3]))
